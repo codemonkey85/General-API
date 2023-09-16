12988cc5ce8f8c3dd33c04bddc0fa828ee07609b
Michael Bond <michael@bondcodes.com>
2023-09-16 18:22:22 -0400

    Cover more situations with default

--- GeneralApi/Endpoints/HelloEndpoints.cs
+++ GeneralApi/Endpoints/HelloEndpoints.cs
@@ -2,21 +2,18 @@
 
 public static class HelloEndpoints
 {
+    private const string World = "World";
+
     public static IEndpointRouteBuilder MapHelloEndpoints(this IEndpointRouteBuilder apiGroup)
     {
         var helloGroup = apiGroup.MapGroup("/hello");
 
-        helloGroup.MapGet("", HelloWorld);
-
-        helloGroup.MapGet("/name", HelloName);
-        helloGroup.MapGet("/name/{name}", (string name) => HelloName(name));
+        helloGroup.MapGet("", HelloName);
+        helloGroup.MapGet("/{name}", (string name) => HelloName(name));
 
         return apiGroup;
     }
 
-    private static Ok<string> HelloWorld() =>
-        TypedResults.Ok("Hello, World!");
-
-    private static Ok<string> HelloName([FromQuery] string name) =>
-        TypedResults.Ok($"Hello, {name}!");
+    private static Ok<string> HelloName([FromQuery] string name = World) =>
+        TypedResults.Ok($"Hello, {(name is { Length: > 0 } ? name : World)}!");
 }
