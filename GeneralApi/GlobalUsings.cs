16782d970b5aa6301f475648dc74603690739755
Michael Bond <michael@bondcodes.com>
2023-10-31 16:01:16 -0400

    Remove unused using

--- GeneralApi/GlobalUsings.cs
+++ GeneralApi/GlobalUsings.cs
@@ -1,5 +1,4 @@
-﻿global using System.Diagnostics.CodeAnalysis;
-global using GeneralApi;
+﻿global using GeneralApi;
 global using GeneralApi.Endpoints;
 global using Microsoft.AspNetCore.Http.HttpResults;
 global using Microsoft.AspNetCore.Mvc;
