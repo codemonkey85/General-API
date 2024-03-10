6c66d3c5e08a83bc6a730494e0e7c11a05160a7b
Michael Bond <michael@bondcodes.com>
2024-03-10 11:57:05 -0400

    Go to playlist by default

--- GeneralApi/Endpoints/AvmEndpoints.cs
+++ GeneralApi/Endpoints/AvmEndpoints.cs
@@ -61,6 +61,6 @@ public class AvmEndpoints : IEndpoint
 
         return vidCode is { Length: > 0 }
             ? $"{vidCodePrefix}{vidCode}"
-            : alanBeckerYouTube;
+            : GetPlaylistUrl();
     }
 }
