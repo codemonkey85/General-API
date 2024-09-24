fc5fa8ac10734b812ef7539c3a201ec78809e814
Michael Bond <michael@bondcodes.com>
2024-09-24 16:45:07 -0400

    Code fixes

--- GeneralApi/GlobalUsings.cs
+++ GeneralApi/GlobalUsings.cs
@@ -1,5 +1,3 @@
-﻿global using GeneralApi;
-global using GeneralApi.Endpoints;
-global using Microsoft.AspNetCore.Http.HttpResults;
+﻿global using Microsoft.AspNetCore.Http.HttpResults;
 global using Microsoft.AspNetCore.Mvc;
 global using static ApiSourceGenerator.EndpointsExtensions;
